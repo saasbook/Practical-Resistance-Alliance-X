55dd666424814ca1cc19d4b0474afea95ca0c9b1
David <encore@berkeley.edu>
2019-03-19 15:26:38 -0700

    Change name

--- app/javascript/components/ToolkitCardView.js
+++ app/javascript/components/ToolkitCardView.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class ToolkitView extends Component {
+export default class ToolkitCardView extends Component {
   constructor(props) {
     super(props);
     this.state = {
