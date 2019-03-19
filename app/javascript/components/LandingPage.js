035c130c4913f9670496b775ee4699659754ded8
David <encore@berkeley.edu>
2019-03-18 18:13:02 -0700

    Add landing page

---
+++ app/javascript/components/LandingPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export class LandingPage extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="text-center landing-center">
+          <h1>Practical Resistance Alliance X</h1>
+          <button type="button" class="btn btn-outline-primary">
+            GET STARTED
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
