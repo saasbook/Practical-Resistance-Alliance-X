0586dc9ae3be9d82f5191b733a729e39bb79eab8
David <encore@berkeley.edu>
2019-03-19 11:49:42 -0700

    Category Page

---
+++ app/javascript/components/CategoryPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CategoryPage extends Component {
+  render() {
+    return (
+      <div>
+        CategoryPage
+      </div>
+    )
+  }
+}
+
+export default CategoryPage
