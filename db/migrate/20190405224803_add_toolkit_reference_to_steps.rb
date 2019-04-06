c2bdc0387f1efe6fc9855e199c8b40df2b1912d4
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2019-04-05 22:40:58 -0700

    Revert "Changes on database and detail view"

--- db/migrate/20190405224803_add_toolkit_reference_to_steps.rb
+++
@@ -1,5 +0,0 @@
-class AddToolkitReferenceToSteps < ActiveRecord::Migration[5.2]
-  def change
-    add_column  :steps, :toolkit_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_225819) do
-
-  create_table "steps", force: :cascade do |t|
-    t.string "content"
-    t.integer "number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "toolkit_id"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "toolkits", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "category"
+    t.text "steps"
     t.text "overview"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
