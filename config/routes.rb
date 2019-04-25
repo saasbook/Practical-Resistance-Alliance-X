bb1b2ae4a9df3232ae71af2be15a703d722a7b1d
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2019-04-25 12:12:22 -0700

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 
   get 'category/index'
@@ -7,6 +11,10 @@ Rails.application.routes.draw do
   get 'category/:kind', to: 'category#show'
   get 'category/:kind/:id', to: 'category#display'
   get 'newtoolkit', to: 'category#new'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
