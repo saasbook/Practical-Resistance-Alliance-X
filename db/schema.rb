b1c161ce0cca2901095140339c85f4e7b0da20d0
David <encore@berkeley.edu>
2019-04-25 16:55:53 -0700

    Add user column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_182041) do
+ActiveRecord::Schema.define(version: 2019_04_25_232303) do
 
   create_table "steps", force: :cascade do |t|
     t.string "content"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_04_25_182041) do
     t.string "lastName"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
