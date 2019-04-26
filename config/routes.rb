4e9bd8abe85f2e68fe017f19af785018e5d75111
Brian Unggul <brianunggul@airbears2-10-142-120-100.airbears2.1918.berkeley.edu>
2019-04-26 10:34:22 -0700

    fixed merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
