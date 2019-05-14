47fc389a65c29f39b27c35875973e98467424b6d
Dawei Feng <12173777+daweifeng@users.noreply.github.com>
2019-05-13 23:03:39 -0700

    rm id (#44)

--- app/javascript/components/NewToolkitButton.js
+++ app/javascript/components/NewToolkitButton.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class NewToolkitButton extends Component {
   render() {
     return (
-      <div className="new-toolkit-button" id="add_toolkit_button">
+      <div className="new-toolkit-button">
         <a href="/newtoolkit">
           <div className="button-content">
             <i className="fa fa-plus-circle" aria-hidden="true" />
